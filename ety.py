2683183613abb25b41693cf877bf08f4d1932d29
Joey Liechty <joey.liechty@unt.edu>
2015-06-09 16:26:50 -0500

    better menu

--- ety.py
+++ ety.py
@@ -1,24 +1,30 @@
 #!/usr/local/bin/python3
-
 from bs4 import BeautifulSoup
-from urllib import request
+import requests
 import sys
+import argparse
 
-if(len(sys.argv) > 1):
+parser = argparse.ArgumentParser(description='Lookup the etymology of a word.')
+parser.add_argument(
+    'word',
+    type=str,
+    nargs='+',
+    help='The word you wish to etymologize.'
+)
+args = parser.parse_args()
 
-    q = sys.argv[1]
-
-    with request.urlopen('http://www.etymonline.com/index.php?term='+q) as response:
-        html = response.read()
+def main():
+    if args.word:
+        q = args.word[0]
+        response = requests.get('http://www.etymonline.com/index.php?term={0}'.format(q))
+        html = response.text
         soup = BeautifulSoup(html, 'lxml')
         for worddef in soup.find_all('dt'):
-            print("")
-            print(worddef.get_text())
-            print(worddef.find_next_sibling().get_text())
-else:
-    print('No query')
+            print('')
+            print(worddef.get_text().strip())
+            print(worddef.find_next_sibling().get_text().strip())
+    else:
+        print('No query')
 
-
-
-
-
+if __name__ == "__main__":
+    main()
